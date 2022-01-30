ba9fe2b0cf64b94edd48dc11c094b02d4e868d07
ulldecorb <ausias.bertran@gmail.com>
2022-01-30 23:40:45 +0100

    Create addTodo component

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -2,31 +2,45 @@
   <div id="app">
     <h1>Todo List</h1>
     <TodoList v-bind:todos="copyTodos" v-on:delete-task="deleteTask" />
+    <AddTodo v-on:add-todo="addTodo" />
   </div>
 </template>
 
 <script>
 import TodoList from './components/TodoList/TodoList.vue'
+import AddTodo from './components/AddTodo/AddTodo.vue';
 // import TodoItem from './components/TodoItem.vue'
 
 export default {
   name: 'App',
   components: {
-    TodoList
+    TodoList, AddTodo
   },
   methods: {
     deleteTask(id) {
       this.todos = this.todos.filter(todo => todo.id !== id);
       this.copyTodos = [...this.todos];
+    },
+    addTodo(todo){
+      this.copyTodos = [...this.todos, todo];
     }
   },
   data() {
     return {
       todos: 
     [
-      {id: '1', task: 'Create VUE app', completed: true},
-      {id: '2', task: 'Create todo components', completed: false},
-      {id: '3', task: 'Test app', completed: false},
+      {
+        id: '5889190e-d1e0-41c1-8f70-4a5f0ba019b7', 
+        task: 'Create VUE app', 
+        completed: true},
+      {
+        id: 'e5dd43e5-e297-4bfe-9fe7-1a28919115c4', 
+        task: 'Create todo components', 
+        completed: false},
+      {
+        id: '68623699-6cdf-405b-af75-27ab367e171b', 
+        task: 'Test app', 
+        completed: false},
     ],
     copyTodos: []
     }
---
+++ todo/src/components/AddTodo/AddTodo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+            <input type="text" v-model="newTask">
+        </form>
+    </div>
+</template>
+<script>
+import { uuid } from 'vue-uuid';
+
+export default {
+    name: 'AddTodo',
+    data(){
+        return {
+            newTask: ''
+        }
+    },
+    methods:{
+        addTodo(e){
+            e.preventDefault();
+
+            const newTodo = {
+                id: uuid.v4(),
+                task: this.newTask,
+                completed: false 
+            }
+
+            this.newTask = '';
+            this.$emit('add-todo', newTodo);
+        }
+    }
+}
+</script>
+<style scoped></style>
